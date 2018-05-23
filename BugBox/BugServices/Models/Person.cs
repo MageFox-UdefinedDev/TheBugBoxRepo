feecefed3ecbfb9dab323ba718f3553c7100ef48
The Mage Fox <magefox.undefined.dev@gmail.com>
2018-05-23 18:53:17 -0500

    Revert "Services"
    This reverts commit 4a94ad021e092faf755849b2224e1573096867a1.

--- BugBox/BugServices/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BugServices.Models
-{
-    public class Person
-    {
-        public int PersonId { get; set; }
-        public string Nick { get; set; }
-        public string Password { get; set; }
-    }
-}
