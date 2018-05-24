e7e17d0333465c1461716a8b8b8ca147a1d4128a
The Mage Fox <magefox.undefined.dev@gmail.com>
2018-05-23 21:43:13 -0500

    Servicess AGAIN

---
+++ BugServices/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+﻿using BugServices.DB;
+using BugServices.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace BugServices.Controllers
+{
+    public class PersonController : ApiController
+    {
+        [HttpGet]
+        public List<Person> GetAllPerson() => Mock.GetAllPersonMock();
+
+        [HttpGet]
+        public Person GetFindPerson(int id) => Mock.GetFindPerson(id);
+    }
+}
---
+++ BugServices/DB/DBServices.cs
@@ -0,0 +1,16 @@
+﻿using BugServices.Models;
+using System.Collections.Generic;
+
+namespace BugServices.DB
+{
+    public interface DBServices
+    {
+        List<Person> GetAllPerson();
+
+        Person FindPerson();
+
+        List<Trap> GetAllTrap();
+
+        Trap FindTrap();
+    }
+}
